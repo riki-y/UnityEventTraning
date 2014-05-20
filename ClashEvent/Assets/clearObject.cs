00245bb46e1dc9eae8eb2ab90a06e0204999c95f
理貴 <b1008244@gmail.com>
2014-05-21 01:10:45 +0900

    いろいろ実装

---
+++ ClashEvent/Assets/clearObject.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class clearObject : MonoBehaviour {
+	int counter = 0;
+	GameObject obj = null;
+
+	void Start () {
+		GameObject.Find("GUI Text").guiText.text = "Collision Event Sample";
+	}
+	
+	void Update () {
+		GameObject cube = GameObject.Find("Cube");
+		if (obj != null) {
+			if (counter++ > 100) {
+				obj.SetActive(true);
+				obj = null;
+			}
+		}
+		try {
+			cube.transform.Rotate(1f, -1f, -1f);
+		} catch(System.NullReferenceException e){}
+
+		if (Input.GetKey(KeyCode.LeftArrow )) {
+			rigidbody.AddForce (new Vector3(-1f, 0,  1f));
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody.AddForce (new Vector3( 1f, 0, -1f));
+		}
+		if (Input.GetKey(KeyCode.UpArrow   )) {
+			rigidbody.AddForce (new Vector3( 1f, 0,  1f));
+		}
+		if (Input.GetKey(KeyCode.DownArrow )) {
+			rigidbody.AddForce (new Vector3(-1f, 0, -1f));
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			if (obj != null) {
+				obj.SetActive(true);
+			}
+			collision.gameObject.SetActive(false);
+			obj = collision.gameObject;
+			counter = 0;
+		}
+	}
+}
---
+++ ClashEvent/Assets/tagObjectClassification.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tagObjectClassification : MonoBehaviour {
+
+	void Start () {
+		GameObject.Find("GUI Text").guiText.text = "Collision Event sample";
+	}
+	
+	void Update () {
+		GameObject cube = GameObject.Find("Cube");
+		cube.transform.Rotate(1f, -1f, -1f);
+		if (Input.GetKey(KeyCode.LeftArrow )) {
+			rigidbody.AddForce(new Vector3(-1f, 0,  1f));
+		}
+		if (Input.GetKey(KeyCode.RightArrow)) {
+			rigidbody.AddForce(new Vector3( 1f, 0, -1f));
+		}
+		if (Input.GetKey(KeyCode.UpArrow   )) {
+			rigidbody.AddForce(new Vector3( 1f, 0,  1f));
+		}
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidbody.AddForce(new Vector3(-1f, 0, -1f));
+		}
+	}
+
+	void OnCollisionEnter(Collision collision) {
+		if (collision.gameObject.tag == "Player") {
+			collision.gameObject.renderer.material.color = Color.yellow;
+		}
+	}
+}
